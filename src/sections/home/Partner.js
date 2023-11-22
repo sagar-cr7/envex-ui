47a986b82b4493a1573ce88566f560585f3fc022
sagar-cr7 <sagarvb7@gmail.com>
2023-11-22 18:38:03 +0530

    form is active

--- src/sections/home/Partner.js
+++ src/sections/home/Partner.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-redeclare */
 // MODULES //
-
+import { useEffect, useState, useRef } from "react";
 // COMPONENTS //
 
 // SECTIONS //
@@ -13,9 +13,46 @@ import styles from "../../styles/sections/home/Partner.module.scss";
 // IMAGES //
 import Mailto from "../../../public/img/home/email.png";
 import Location from "../../../public/img/home/location.png";
+import close from "../../../public/img/home/close.svg";
 
 /** Home Hero Section */
 export default function Partner() {
+	const contactForm = useRef();
+	const clearFormData = () => {
+		contactForm.current.reset();
+	};
+
+	/** openPopup function */
+	function openPopup() {
+		const popup = document.getElementById("popup");
+		popup.style.display = "block";
+		contactForm.current.reset();
+	}
+	/** closePopup function */
+	function closePopup() {
+		const popup = document.getElementById("popup");
+		popup.style.display = "none";
+	}
+	/** handleSubmit function */
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		const scriptURL =
+			"https://script.google.com/macros/s/AKfycbzqnRB2hsqy8oh_4JqjptJurGUGtgnLoiEQsiFjsXa2OnBnJmnVlkeKqzpbIMf9LBmHAQ/exec";
+
+		fetch(scriptURL, {
+			method: "POST",
+			body: new FormData(contactForm.current),
+		});
+
+		fetch(scriptURL, {
+			method: "POST",
+			body: new FormData(contactForm.current),
+		})
+			// .then((response) => alert("You have successfully submitted."))
+			.then((response) => openPopup())
+			.catch((error) => console.error("Error!", error.message));
+	};
+
 	return (
 		<div className={`${styles.partner_section} ptb_80`} name="contact">
 			<div className="container">
@@ -45,7 +82,13 @@ export default function Partner() {
 						</ul>
 					</div>
 					<div className={`${styles.form}`}>
-						<form name="Home Contact" method="POST">
+						<form
+							ref={contactForm}
+							onSubmit={handleSubmit}
+							name="Home Contact"
+							method="POST"
+							id="contactForm"
+						>
 							<div className={`${styles.form_group} fadeInUp`}>
 								<div className={`${styles.form_field}`}>
 									<input type="text" placeholder="Full Name" name="Name" required />
@@ -72,6 +115,15 @@ export default function Partner() {
 							</div>
 						</form>
 					</div>
+				</div>
+			</div>
+			<div id="popup" class={styles.popup}>
+				<div class={styles.popup_content}>
+					<span class={styles.close_btn} onClick={closePopup}>
+						<img src={close.src} alt="" />
+					</span>
+					<p className="text_reg">Thank You for contacting us.</p>
+					<p className="text_reg">We will get back to you shortly.</p>
 				</div>
 			</div>
 		</div>
