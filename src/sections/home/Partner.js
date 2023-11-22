d82737e9fb9343c71430a17ed8b3c911a5385fe3
pramodchauhanting <108465153+pramodchauhanting@users.noreply.github.com>
2023-11-22 14:44:19 +0530

    changes done

--- src/sections/home/Partner.js
+++ src/sections/home/Partner.js
@@ -20,7 +20,7 @@ export default function Partner() {
 		<div className={`${styles.partner_section} ptb_80`} name="contact">
 			<div className="container">
 				<div className={`${styles.partner}`}>
-					<div className={`${styles.info} pb_50`}>
+					<div className={`${styles.info} pb_30`}>
 						<div className={`${styles.title} section_title color_white`}>
 							Partner with us!
 						</div>
